8bf9a328c3f2a09f197c0e24d45f8f543ed8b531
kubaorluk <kubaorluk@gmail.com>
2022-11-11 10:23:44 +0100

    Dodano password validator

--- database/models/User.js
+++ database/models/User.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
+const { isValidPassword } = require('mongoose-custom-validators')
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -13,7 +14,11 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: isValidPassword,
+            message: 'Password must have: 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character and be at least 10 characters long.'
+        }
     }
 })
 
