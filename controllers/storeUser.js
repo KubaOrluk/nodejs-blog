4dd766a6f8e953417a242c80719ffcbb0445d295
kubaorluk <kubaorluk@gmail.com>
2022-11-15 10:07:06 +0100

    Poprawka literówki

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
     User.create(req.body, (error, user) => {
         if (error) {
             if(!error.errors){
-                registrationErrors = 'User with those credentials already exist'
+                registrationErrors = 'User with those credentials already exists'
             }
             else{
                 registrationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
